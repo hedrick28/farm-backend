ce3050550132aa146f2a42bfb6b9d5349f78a648
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2022-10-28 10:01:34 +0800

    add product page

--- api/target/farm-to-market/src/routes/farmer.js
+++ api/target/farm-to-market/src/routes/farmer.js
@@ -3,11 +3,15 @@ const Dashboard = React.lazy(() => import("../pages/retailer/Dashboard"));
 const Profile = React.lazy(() => import("../pages/retailer/Profile"));
 const EditCropPage = React.lazy(() => import("../pages/crop/EditCropPage"));
 const CropPage = React.lazy(() => import("../pages/crop/CropPage"));
+const AddPRoduct = React.lazy(() => import("../pages/retailer/AddProduct"));
+const Products = React.lazy(() => import("../pages/retailer/Products"));
 const routes = [
   { path: "/dashboard", exact: true, element: Dashboard },
   { path: "/profile", element: Profile },
   { path: "/editCrop/:id", element: EditCropPage },
   { path: "/crops", element: CropPage },
+  { path: "/product/add", element: AddPRoduct },
+  { path: "/products", element: Products },
 ];
 
 export default routes;
